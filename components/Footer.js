ff59824192ab3e202b71280a562efd9c3106ed80
Arav <aravdave@gmail.com>
2022-12-29 17:03:45 -0500

    New portfolio website

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,7 @@
-import styles from './Footer.module.css'
-
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
+      <footer className="block bg-cyan-900 max-w-full inset-x-0 bottom-0 left-0 p-4 text-center text-white text-sm">
         Copyright © {new Date().getFullYear()} by Arav Dave
       </footer>
     </>
