2dd517a2d0058da13869f3e4bdafd951465f8884
Arav Dave <aravdave@gmail.com>
2023-12-18 18:32:32 -0500

    Updated profile image, and devpost link

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <>
-      <footer className="block bg-cyan-900 max-w-full inset-x-0 bottom-0 left-0 p-4 text-center text-white text-sm">
+      <footer className="block bg-sky-900 max-w-full inset-x-0 bottom-0 left-0 p-4 text-center text-white text-sm">
         Copyright © {new Date().getFullYear()} by Arav Dave
       </footer>
     </>
-  )
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-cyan-900'>
+      <body className="bg-sky-900">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
