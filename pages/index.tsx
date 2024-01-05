d3a79a982b9a1c3ad0f7e4be7d9e8fd551e78e3c
Arav Dave <aravdave@gmail.com>
2024-01-04 22:45:53 -0500

    Added Photo Assassin and Hangrs to Notable Projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -181,7 +181,7 @@ export default function Home() {
                 . I&apos;m always eager to learn something new and look forward
                 to new opportunities. In my free time, you will find me out on
                 long runs, vibing to indie rock music, or exploring DC with
-                friends. Feel free to reach out about anything!
+                friends. Feel free to reach out!
               </div>
             </div>
           </div>
@@ -233,6 +233,115 @@ export default function Home() {
                   target="_blank"
                   rel="noreferrer"
                   href="https://github.com/PCbitcamp22"
+                >
+                  <svg
+                    viewBox="0 0 100 100"
+                    className="ease-in-out delay-0 duration-75 hover:scale-125 fill-black"
+                    width="40"
+                    height="40"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="drop-shadow-2xl rounded-md flex flex-col items-center bg-amber-100">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://pa.junion44.rocks/"
+              >
+                <Image
+                  className="rounded-t-md"
+                  src="/images/photo-assassin.png"
+                  quality="90"
+                  width={249}
+                  height={166}
+                  alt="Photo Assassin screenshot"
+                />
+              </a>
+              <a
+                className="hover:underline underline-offset-4 hover:decoration-sky-600 mt-4 text-2xl font-sans font-semibold text-black"
+                rel="noreferrer"
+                target="_blank"
+                href="https://devpost.com/software/photo-assassin"
+              >
+                Photo Assassin
+              </a>
+              <div className="flex flex-row items-center space-x-8 mt-2 mb-4 mr-3 ml-3">
+                <a
+                  aria-label="Devpost Link"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://devpost.com/software/photo-assassin"
+                >
+                  <Image
+                    className="ease-in-out duration-75 hover:scale-125"
+                    src="/images/devpost-logo.svg"
+                    width={100}
+                    height={30}
+                    alt="Devpost logo"
+                  />
+                </a>
+                <a
+                  aria-label="Github Link"
+                  className="self-center justify-center items-center mr-3"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/orgs/AAAP-Bitcamp/repositories"
+                >
+                  <svg
+                    viewBox="0 0 100 100"
+                    className="ease-in-out delay-0 duration-75 hover:scale-125 fill-black"
+                    width="40"
+                    height="40"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            {/*  Hangrs */}
+            <div className="drop-shadow-2xl rounded-md flex flex-col items-center bg-amber-100">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/aravdave/Hangrs"
+              >
+                <Image
+                  className="rounded-t-md"
+                  src="/images/hangrs-logo.jpeg"
+                  quality="90"
+                  width="170"
+                  height="170"
+                  alt="Codle screenshot"
+                />
+              </a>
+              <a
+                className="hover:underline underline-offset-4 hover:decoration-sky-600 mt-4 text-2xl font-sans font-semibold text-black"
+                rel="noreferrer"
+                target="_blank"
+                href="https://github.com/aravdave/Hangrs"
+              >
+                Hangrs
+              </a>
+              <div className="flex flex-row items-center space-x-8 mt-2 mb-4 mr-3 ml-6">
+                <a
+                  aria-label="Github Link"
+                  className="self-center justify-center items-center mr-3"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/aravdave/Hangrs"
                 >
                   <svg
                     viewBox="0 0 100 100"
